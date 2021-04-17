2434e6c483c59b58a063fe297f2e9821df0f27b9
Jennifer Kruk <jenkruk@gmail.com>
2021-04-17 11:06:30 -0700

    adding Global State Objects

---
+++ client/src/__tests__/reducers.test.js
@@ -0,0 +1,48 @@
+import { reducer } from '../utils/reducers';
+
+// import our actions
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+  } from '../utils/actions';
+  
+  // create a sample of what our global state will look like
+  const initialState = {
+    products: [],
+    categories: [{ name: 'Food' }],
+    currentCategory: '1',
+  };
+
+  //Test for updating the UPDATE_PRODUCTS action to see if we can add a product to the products array
+  test('UPDATE_PRODUCTS', () => {
+    let newState = reducer(initialState, {
+      type: UPDATE_PRODUCTS,
+      products: [{}, {}]
+    });
+  
+    expect(newState.products.length).toBe(2);
+    expect(initialState.products.length).toBe(0);
+  });
+
+//   test for the UPDATE_CATEGORIES action 
+  test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+      type: UPDATE_CATEGORIES,
+      categories: [{}, {}]
+    });
+  
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+  });
+
+//   test for the UPDATE_CURRENT_CATEGORY action
+  test('UPDATE_CURRENT_CATEGORY', () => {
+    let newState = reducer(initialState, {
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: '2'
+    });
+  
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
+  });
